b01786a548eabd21e65e3bc8f33fd6263c070453
WangHaolong <wanghaolong0@126.com>
2020-01-07 13:27:56 +0800

    do some exersises

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 /**
-My first Java program
-*/
-public class Hello{
-    public static void main(String[]args){
+ * My first Java program
+ */
+public class Hello {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
---
+++ Printf_test.java
@@ -0,0 +1,6 @@
+public class Printf_test{
+	public static void main(String[]args){
+		int d=100;
+		System.out.printf("%d",d);
+	}
+}
